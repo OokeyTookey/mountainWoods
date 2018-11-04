026e9eef953d681b867845c85fa4794b12eab6d9
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2018-11-04 13:05:43 +0400

    Added more assets on the main scene

---
+++ mountainWoods/Assets/Scripts/BehaviourTree/BehaviourTree.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NSBehaviourTree
+{
+    public class BehaviourTree : MonoBehaviour
+    {
+        Node parentNode = new Node();
+        int index = 0;
+
+        int Index
+        {
+            get
+            {
+                return index;
+            }
+
+            set
+            {
+                if(value <= parentNode.nodes.Count -1)
+                    index = value;
+            }
+        }
+
+        void Start()
+        {
+          /*  parentNode.nodes.Add(new Eat());
+            parentNode.nodes.Add(new Drink());
+            parentNode.nodes.Add(new Open());
+            parentNode.nodes.Add(new Die());
+
+            ((Eat)parentNode.nodes[0]).Behaviour();*/
+        }
+
+        void Update()
+        {
+            //Debug.Log(Index);
+
+            /*if (parentNode.nodes[Index].success)
+            {
+                Index++;
+            }
+
+            else if (parentNode.nodes[Index].failure)
+            {
+                parentNode.failure = true;
+            }*/
+
+            /*Debug.Log("<color=green> sucess </color>"+ parentNode.success);
+            Debug.Log("<color=red> fairure </color> " + parentNode.failure);*/
+           // Debug.Log(parentNode.returning);
+        }
+    }
+}
--- mountainWoods/Assets/Scripts/PlayerController.cs
+++ mountainWoods/Assets/Scripts/PlayerController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 100.0f;
 
-	// Use this for initialization
-	void Start ()
+    public float speed;
+    public float jumpForce;
+    public Rigidbody playerRB;
+
+    // Use this for initialization
+    void Start ()
     {
         // this function is called to lock the cursor in game mode
         Cursor.lockState = CursorLockMode.Locked;
@@ -15,13 +18,38 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float moveHor = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        float moveVert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        transform.Translate(moveHor, 0, moveVert);
 
         if(Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
-	} 
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRB.AddForce(transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRB.AddForce(-transform.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRB.AddForce(-transform.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRB.AddForce(transform.right * speed);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.AddForce(transform.up * jumpForce);
+        }
+    }
 }
