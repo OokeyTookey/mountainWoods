2bdb8a3052957d9236503ebc0d7b3f14a37694b9
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2018-11-11 11:32:43 +0400

    The sheep has some basic AI, Aka: it will walk into an object and walk the other way

---
+++ mountainWoods/Assets/Scripts/SheepAi.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAi : MonoBehaviour
+{
+
+    float currentRotation;
+    float oppositeRotation;
+    bool collidedOrNot = false;
+    float turnSpeed=1;
+    
+
+    void Start()
+    {
+            currentRotation = transform.rotation.eulerAngles.y;
+    }
+
+    void Update()
+    {
+        transform.Translate((Vector3.forward * Time.deltaTime));
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "sheepObjectCollision")
+        {
+            Debug.Log("Sheep Collided");
+            currentRotation = -currentRotation;
+            transform.Rotate(0, currentRotation, 0);
+        }
+    }
+
+ 
+}
