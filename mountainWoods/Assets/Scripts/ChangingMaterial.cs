4c841bd6b5c8ad270542d49e83fdc4371ff86d77
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2018-11-18 23:49:41 +0400

    Clouds are now functional with bobbing motion. Not only that! Now we can change the colour of grene object in the scene! (Not icnluding grass- we dont talk about that). FURTHURE MORE! We now have a sheep which MOVES and if you are careful, you can run him off the island!! :D //Im loosing my mind.

---
+++ mountainWoods/Assets/Scripts/ChangingMaterial.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingMaterial : MonoBehaviour
+{
+
+    [HeaderAttribute("[trees]")]
+
+    public Material[] materials;
+    MeshRenderer[] meshrenders;
+    GameObject[] greenObjects;
+
+    [HeaderAttribute("[Islands]")]
+    GameObject[] islandObjects;
+    public Material[] IslandMaterials;
+
+    void Start()
+    {
+        islandObjects = GameObject.FindGameObjectsWithTag("island");
+        greenObjects = GameObject.FindGameObjectsWithTag("green");
+        meshrenders = new MeshRenderer[greenObjects.Length + islandObjects.Length];
+
+        for (int i = 0; i < greenObjects.Length + islandObjects.Length; i++)
+        {
+            if (i < greenObjects.Length)
+                meshrenders[i] = greenObjects[i].GetComponent<MeshRenderer>();
+            else
+                meshrenders[i] = islandObjects[(greenObjects.Length + islandObjects.Length) - i - 1].GetComponent<MeshRenderer>();
+        }
+
+        print("??!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            for (int i = 0; i < meshrenders.Length; i++)
+            {
+                print("ispressed");
+
+                if (meshrenders[i].tag == "green")
+                    meshrenders[i].materials = materials;
+
+                else if (meshrenders[i].tag == "island")
+                    meshrenders[i].materials = IslandMaterials;
+            }
+        }
+    }
+
+
+
+
+}
--- mountainWoods/Assets/Scripts/Flee.cs
+++ mountainWoods/Assets/Scripts/Flee.cs
@@ -8,21 +8,65 @@ public class Flee : MonoBehaviour {
     Vector3 desiredVelo;
     public float maxVelo;
     Vector3 steering;
-    
+
+    public GameObject cube;
+
 	void Start ()
     {
         RB = GetComponent<Rigidbody>();
+
 	}
 	
 	
 	void Update ()
     {
-        desiredVelo = (targetPosition.position - transform.position).normalized * maxVelo;
+        /*desiredVelo = (targetPosition.position - transform.position).normalized * maxVelo;
         steering = desiredVelo - RB.velocity;
+        RB.velocity = Vector3.ClampMagnitude(RB.velocity, 3);
 
-        RB.AddForce(steering);
- 
+        RB.AddForce(-steering);*/
+
         //Truncate is clamping- arrival 
         // look where current velocity is
+        //--------------------------------------------------------------------------------------------------------------------------
+
+        //Vector3 dir = (targetPosition.position - transform.position).normalized;
+
+        //desiredVelo = dir * maxVelo;
+        //steering = desiredVelo - RB.velocity;
+        //RB.velocity = Vector3.ClampMagnitude(RB.velocity, 5);
+
+        //transform.LookAt(new Vector3(targetPosition.position.x, targetPosition.position.y, targetPosition.position.z));
+
+        //transform.position -= transform.forward * 2.0f * Time.deltaTime;
+        //RB.AddForce(-desiredVelo);
+
+        /* Vector3 dir = new Vector3(targetPosition.position.x, targetPosition.position.y, targetPosition.position.z);
+         dir.Normalize();
+
+         transform.LookAt(new Vector3(-dir.x, -dir.y, -dir.z));
+         Debug.DrawRay(transform.position, new Vector3(-dir.x, -dir.y, -dir.z) * 1000);*/
+
+        //transform.rotation = targetPosition.rotation;
+
+
+        if (Vector3.Distance(transform.position, targetPosition.position) < 10)
+        {
+            Vector3 dir = (transform.position - targetPosition.transform.position);
+            dir.Normalize();
+
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+
+            angle = Mathf.Lerp(angle - 45, angle + 45, Time.deltaTime * 10.0f);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+            print(angle);
+
+            //transform.LookAt(new Vector3(dir.x, 0.075f, dir.z) * 1000);
+
+            //Debug.DrawRay(transform.position, dir * 1000);
+
+            transform.position += transform.forward * 2.0f * Time.deltaTime;
+        }
+        
     }
 }
