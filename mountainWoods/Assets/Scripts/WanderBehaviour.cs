ba316b2cc2d7a79826502df65372c07dacbcf8cf
PRIE\priea <p.sagar@saedubai.com>
2018-12-03 23:57:22 +0400

    Some basic changes to my test1 scene.

--- mountainWoods/Assets/Scripts/WanderBehaviour.cs
+++ mountainWoods/Assets/Scripts/WanderBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WanderBehaviour : MonoBehaviour {
     public float wanderTime;
     public float movementSpeed;
+    public float circleDistance;
+    private Vector3 velocity;
     //public float wanderAngle;
    
      // Use this for initialization
@@ -32,6 +34,17 @@ public class WanderBehaviour : MonoBehaviour {
     {
         transform.eulerAngles = new Vector3(0, Random.Range(0, 360), 0);
     }
-     }
+
+    //private Vector3 wander()
+    //{
+    //    var circleCenter = new Vector3();
+    //    circleCenter = velocity.normalized;
+    //    circleCenter.Scale(velocity);
+
+    //    var displacement = new Vector3(0, -1);
+    //    displacement.Scale(velocity);
+
+    //}
+}
 
   
