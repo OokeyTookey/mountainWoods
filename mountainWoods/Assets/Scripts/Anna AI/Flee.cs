a6b478c350b3e14cafc19f2038d510e33f10e163
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2018-12-05 17:51:20 +0400

    Created "Anna AI" scene where I can work on different behaviours. Added first polished version of FLEE. Just working on a smooth arrival now.

--- mountainWoods/Assets/Scripts/Anna AI/Flee.cs
+++ mountainWoods/Assets/Scripts/Anna AI/Flee.cs
@@ -2,33 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Flee : MonoBehaviour {
-    public Transform targetPosition;
+public class Flee : MonoBehaviour
+{
+
     Rigidbody RB;
-    Vector3 desiredVelo;
-    public float maxVelo;
     Vector3 steering;
+    Vector3 desiredVelo;
+    float distanceFrom;
+    float desiredDistance;
+    float slowingRadius;
+    public float maxVelo;
+    public Transform targetPosition;
 
-    public GameObject cube;
-
-	void Start ()
+    void Start()
     {
         RB = GetComponent<Rigidbody>();
+    }
 
-	}
-	
-	
-	void Update ()
+    void Update()
     {
-        desiredVelo = (targetPosition.position - transform.position).normalized * maxVelo;
-        steering = desiredVelo - RB.velocity;
-        RB.velocity = Vector3.ClampMagnitude(RB.velocity, 3);
+        desiredDistance = desiredVelo.magnitude;
 
-        RB.AddForce(steering);
+        //Get the desired velocity for flee by minusing the target positions (in this case the player) from the attached objects position
+        desiredVelo = (transform.position - targetPosition.position).normalized * maxVelo;
+        steering = desiredVelo - RB.velocity; //Sets the steering behaviour by minusing
+        RB.velocity = Vector3.ClampMagnitude(RB.velocity, 3); //sets the velocity of the 
+        RB.AddForce(steering); //Moves the character based on the set steering behaviour
 
-        //Truncate is clamping- arrival 
-        // look where current velocity is
-     
-        
+        distanceFrom = (transform.position - targetPosition.position).magnitude; //Calculates the distance between the sheep and position
+
+       /* if (desiredDistance < slowingRadius)
+        {
+            desiredVelo = desiredVelo.normalized * maxVelo * (desiredDistance / slowingRadius);
+        }
+        else
+        {
+            // Outside the slowing area.
+            desiredVelo = desiredVelo.normalized * maxVelo;
+        }*/
     }
 }
