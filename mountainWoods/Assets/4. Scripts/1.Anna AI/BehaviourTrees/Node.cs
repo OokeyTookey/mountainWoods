84b329b1cc3f3de5200011b98f7ad4c704fc71b1
ANNATOOKEY\annat <anna.tookey@hotmail.com>
2019-01-14 13:36:15 +0400

    Bug fixing/ code review

--- mountainWoods/Assets/4. Scripts/1.Anna AI/BehaviourTrees/Node.cs
+++ mountainWoods/Assets/4. Scripts/1.Anna AI/BehaviourTrees/Node.cs
@@ -6,7 +6,6 @@ public class Node
 {
     public List<Node> nodes = new List<Node>();
 
-
     public enum Result { running, success, failure } //Enum of the different states which each node can have
 
     public Result previousResult;
